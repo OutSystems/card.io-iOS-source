d92fa420f7d5c3c4e71e8c2b0e4972cd3c9fd584
João Ferreira <joao.ferreira@outsystems.com>
2019-12-12 17:53:07 +0000

    build: disable linking of i386 and x86_64 architectures
    They are desktop architectures which serve no purpose in a mobile
    context, other than bloating the library file size over GitHub's
    100MB limit.
    
    References https://outsystemsrd.atlassian.net/browse/RNMT-3424

--- fabfile.py
+++ fabfile.py
@@ -195,8 +195,6 @@ def build(outdir=None, device_sdk=None, simulator_sdk=None, **kwargs):
                 # in Xcode 4.5 GM, xcrun selects the wrong lipo to use, so circumventing xcrun for now :(
                 lipo_cmd = "`xcode-select -print-path`/Toolchains/XcodeDefault.xctoolchain/usr/bin/lipo " \
                            "           {archive}/{libname}" \
-                           "           -arch i386 {i386}/{libname}" \
-                           "           -arch x86_64 {x86_64}/{libname}" \
                            "           -create" \
                            "           -output {universal}/{libname}".format(libname=env.libname, **arch_build_dirs)
                 local(lipo_cmd)
